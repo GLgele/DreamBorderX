a1217d679f88fd551216281704d4fb7dd4a51218
GLgele <60538556+GLgele@users.noreply.github.com>
2020-07-06 15:55:35 +0800

    Update UpNew.js

--- js/UpNew.js
+++ js/UpNew.js
@@ -4,7 +4,7 @@ if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
 else {// code for IE6, IE5
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
-xmlhttp.open("GET", "../xml/UpNews.xml", false);
+xmlhttp.open("GET", "xml/UpNews.xml", false);
 xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;
 
