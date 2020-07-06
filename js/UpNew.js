234264c3bb28a693f26a4fe5d393c30272072429
GLgele <60538556+GLgele@users.noreply.github.com>
2020-07-06 16:22:33 +0800

    Update UpNew.js

--- js/UpNew.js
+++ js/UpNew.js
@@ -8,15 +8,9 @@ xmlhttp.open("GET", "xml/UpNews.xml", false);
 xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;
 
-document.getElementById("news1").innerHTML =
-xmlDoc.getElementsByTagName("news1")[0].childNodes[0].nodeValue;
-document.getElementById("news2").innerHTML =
-xmlDoc.getElementsByTagName("news2")[0].childNodes[0].nodeValue;
-document.getElementById("news3").innerHTML =
-xmlDoc.getElementsByTagName("news3")[0].childNodes[0].nodeValue;
-document.getElementById("update1").innerHTML =
-xmlDoc.getElementsByTagName("update1")[0].childNodes[0].nodeValue;
-document.getElementById("update2").innerHTML =
-xmlDoc.getElementsByTagName("update2")[0].childNodes[0].nodeValue;
-document.getElementById("update3").innerHTML =
-xmlDoc.getElementsByTagName("update3")[0].childNodes[0].nodeValue;
+document.getElementById("news1").innerHTML =xmlDoc.getElementsByTagName("news1")[0].childNodes[0].nodeValue;
+document.getElementById("news2").innerHTML =xmlDoc.getElementsByTagName("news2")[0].childNodes[0].nodeValue;
+document.getElementById("news3").innerHTML =xmlDoc.getElementsByTagName("news3")[0].childNodes[0].nodeValue;
+document.getElementById("update1").innerHTML =xmlDoc.getElementsByTagName("update1")[0].childNodes[0].nodeValue;
+document.getElementById("update2").innerHTML =xmlDoc.getElementsByTagName("update2")[0].childNodes[0].nodeValue;
+document.getElementById("update3").innerHTML =xmlDoc.getElementsByTagName("update3")[0].childNodes[0].nodeValue;
