c74534b98fcbad842ab4f9b38ba26575e2b1e94f
GLgele <ai_minecraft@163.com>
2020-07-08 14:45:30 +0800

    Update UpNew.js

--- ljx/js/UpNew.js
+++ ljx/js/UpNew.js
@@ -1,7 +1,7 @@
-if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+if (window.XMLHttpRequest) {                                                           // code for IE7+, Firefox, Chrome, Opera, Safari 
     xmlhttp = new XMLHttpRequest();
 }
-else {// code for IE6, IE5
+else {                                                                                // code for IE6, IE5
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 }
 xmlhttp.open("GET", "https://glgele.github.io/DreamBorderX/xml/UpNews.xml", false);
